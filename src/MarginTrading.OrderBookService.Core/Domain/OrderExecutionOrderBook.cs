1257a8839f4327bf2e476567f15a1214f842d52d
Andrey Anokhin <yashmel@mail.ru>
2019-07-08 16:34:14 +0300

    LT-1541: license and copyrights

--- src/MarginTrading.OrderBookService.Core/Domain/OrderExecutionOrderBook.cs
+++ src/MarginTrading.OrderBookService.Core/Domain/OrderExecutionOrderBook.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2019 Lykke Corp.
+// See the LICENSE file in the project root for more information.
+
 using MarginTrading.OrderBookService.Core.Domain.Abstractions;
 
 namespace MarginTrading.OrderBookService.Core.Domain
